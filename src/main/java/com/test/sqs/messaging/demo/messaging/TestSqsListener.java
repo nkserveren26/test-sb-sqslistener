d91f88d5c70d2935cb8187bdaf940551828f3c97
nkserveren26 <norinoritoshi@outlook.jp>
2025-07-24 08:42:33 +0900

    例外処理設定。

--- src/main/java/com/test/sqs/messaging/demo/messaging/TestSqsListener.java
+++ src/main/java/com/test/sqs/messaging/demo/messaging/TestSqsListener.java
@@ -11,11 +11,15 @@ public class TestSqsListener {
         System.out.println("TestSqsListener インスタンスを生成");
     }
 
-    @SqsListener(value = "test-queue-spring-boot", acknowledgementMode = SqsListenerAcknowledgementMode.MANUAL)
+    @SqsListener(value = "test-queue-spring-boot", acknowledgementMode = SqsListenerAcknowledgementMode.ON_SUCCESS)
     public void receiveMessageFromQueue (String message) {
-        System.out.println("メッセージをポーリングします");
-        System.out.println("Received a message from SQS: "+ message);
-        // 意図的に例外をスロー
-        //throw new RuntimeException("これは意図的な例外です");
+        try {
+            System.out.println("メッセージをポーリングします");
+            System.out.println("Received a message from SQS: "+ message);
+            // 意図的に例外をスロー
+            throw new RuntimeException("これは意図的な例外です");
+        } catch (RuntimeException e) {
+            System.out.println("例外が発生しました：" + e);
+        }
     }
 }
